8ce429e0f7f0a976e770c1c511faa4841b088a60
Ivan Pasechnik <vonorapunzel@yandex.ru>
2022-03-01 10:12:29 +0300

    recode

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,8 +1,17 @@
+const { requestError } = require('../utils/errorConstant');
+
 const errorHandler = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'Ошибка на стороне сервера' : message,
-  });
+  if (!err.status) {
+    const { statusCode = 500, message } = err;
+
+    res.status(statusCode).send(
+      statusCode === 500 ? { message: requestError.serverError.ERROR } : message,
+    );
+  } else {
+    res.status(err.status).send(err.message);
+    return;
+  }
+
   next();
 };
 
